d064fd473ad42df38b305a4fb98b1a56ebbd1c71
Tống Đức Duy <tongducduy4@gmail.com>
2024-01-31 19:55:04 +0700

    up

--- bai1.js
+++ bai1.js
@@ -1,30 +1,3 @@
-// const list = [
-//     {
-//       id: 1,
-//       content: 'Chim to chim nho',
-//     },
-//     {
-//       id: 2,
-//       content: 'Anh em sky oi',
-//     },
-//     {
-//       id: 3,
-//       content: 'Say oh yeah',
-//     },
-//     {
-//       id: 4,
-//       content: 'yeah yeah db',
-//     }
-// ]; 
-// const mappedContent = list.map(item => item.content);
-
-// const hcnconElements = document.querySelectorAll('.hcncon');
-
-// mappedContent.forEach((content, index) => {
-//     if (index < hcnconElements.length) {
-//         hcnconElements[index].textContent = content;
-//     }
-// });
     document.addEventListener("DOMContentLoaded", function() {
         const list = [
             {
@@ -41,13 +14,13 @@
             },
             {
             id: 4,
+                                                                                     
             content: 'yeah yeah db',
             }
         ]; 
-        const mappedContent = list.map(item => item.content);
-        const myDiv = document.getElementById("hcncon");
-
-        myDiv.innerHTML = mappedContent.map(item => `<p>${item}</p>`).join('');
+        const divCha = document.querySelector('#hcn1');
+        const children = list.map(item => `<div class="mau-hong">${item.content}</div>`).join('');
+        divCha.innerHTML = children
     });
 
 
